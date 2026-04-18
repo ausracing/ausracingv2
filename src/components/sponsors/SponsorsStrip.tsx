ca756381ddc21a76d47b6d7e6e9f74c5463e2c7f
Yasmeen1506 <yasmeenhk06@gmail.com>
2026-04-18 21:19:36 +0400

    sponsor slider

--- src/components/sponsors/SponsorsStrip.tsx
+++ src/components/sponsors/SponsorsStrip.tsx
@@ -1,38 +1,30 @@
 "use client"
 
 import * as React from "react"
+import Image from "next/image"
 import AutoScroll from "embla-carousel-auto-scroll"
 import {
   Carousel,
   CarouselContent,
   CarouselItem,
-} from "./ui/carousel"
+} from "../ui/carousel"
 
-const sponsors = [
-  "SPONSOR 1",
-  "SPONSOR 2",
-  "SPONSOR 3",
-  "SPONSOR 4",
-  "SPONSOR 5",
-  "SPONSOR 6",
-  "SPONSOR 7",
-  "SPONSOR 8",
-  "SPONSOR 9",
-  "SPONSOR 10",
-  "SPONSOR 11",
-  "SPONSOR 12",
-  "SPONSOR 13",
-  "SPONSOR 14",
-  "SPONSOR 15",
-  "SPONSOR 16",
-  "SPONSOR 17",
-  "SPONSOR 18",
+const baseSponsors = [
+  { name: "AGMC", src: "/partners/agmc.png" },
+  { name: "Ansys", src: "/partners/ansys1.png" },
+  { name: "Automech", src: "/partners/automech2.png" },
+  { name: "DEWESoft", src: "/partners/dewesoft.png" },
+  { name: "Fluid Codes", src: "/partners/fluidcodes1.png" },
+  { name: "Juma Al Majid", src: "/partners/juma.png" },
+  { name: "SRTI Park", src: "/partners/srti.png" },
 ]
 
+const sponsors = [...baseSponsors, ...baseSponsors, ...baseSponsors]
+
 export default function SponsorsStrip() {
-  const autoScroll = React.useRef(
+  const plugin = React.useRef(
     AutoScroll({
-      speed: 0.8,
+      speed: 0.9,
       startDelay: 0,
       stopOnInteraction: false,
       stopOnMouseEnter: true,
@@ -41,31 +33,36 @@ export default function SponsorsStrip() {
   )
 
   return (
-    <section className="w-full overflow-hidden bg-black py-5">
-      <div className="w-full px-4 sm:px-6 md:px-8">
-        <Carousel
-          plugins={[autoScroll.current]}
-          opts={{
-            align: "start",
-            loop: true,
-            dragFree: true,
-          }}
-          className="w-full"
-        >
-          <CarouselContent className="-ml-3 md:-ml-4">
-            {sponsors.map((sponsor) => (
-              <CarouselItem
-                key={sponsor}
-                className="pl-3 md:pl-4 basis-1/2 sm:basis-1/3 md:basis-1/4 lg:basis-1/5 xl:basis-1/6"
-              >
-                <div className="flex h-12 w-full items-center justify-center rounded-xl border border-white/10 bg-[#121316] px-3 text-center text-[10px] sm:text-[11px] font-semibold uppercase tracking-[0.18em] text-zinc-500 transition-all duration-300 hover:border-brand/30 hover:text-zinc-200">
-                  {sponsor}
+    <section className="w-full overflow-hidden bg-[#0a0a0a] py-4">
+      <Carousel
+        plugins={[plugin.current]}
+        opts={{
+          align: "start",
+          loop: true,
+          dragFree: true,
+        }}
+        className="w-full"
+      >
+        <CarouselContent className="-ml-3 md:-ml-4">
+          {sponsors.map((sponsor, index) => (
+            <CarouselItem
+              key={`${sponsor.name}-${index}`}
+              className="pl-3 md:pl-4 basis-[65%] sm:basis-[40%] md:basis-[28%] lg:basis-[20%] xl:basis-[15%]"
+            >
+              <div className="flex h-20 w-full items-center justify-center rounded-xl border border-white/10 bg-[#0f0f10] px-6 transition-all duration-300 hover:border-[#fbb03a]/40 hover:bg-[#141414] hover:shadow-[0_0_20px_rgba(251,176,58,0.2)]">
+                <div className="relative h-16 w-full">
+                  <Image
+                    src={sponsor.src}
+                    alt={sponsor.name}
+                    fill
+                    className="object-contain grayscale opacity-70 transition duration-300 hover:grayscale-0 hover:opacity-100"
+                  />
                 </div>
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-        </Carousel>
-      </div>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
     </section>
   )
 }
