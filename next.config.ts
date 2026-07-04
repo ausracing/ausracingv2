c566596e11b688a1a0601e524440884aac61ed56
Am-GJ <RealRyanGosling@proton.me>
2026-07-04 11:27:34 +0400

    Update next.config.ts (enviro point to pocketbase)

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,11 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
+        protocol: "https",
+        hostname: "pocketbase.justdomain.link",
+        pathname: "/api/files/**",
+      },
+      {
         protocol: "http",
         hostname: "192.168.8.210",
         port: "9090",
